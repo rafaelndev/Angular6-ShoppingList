dd9dc5a2286c05af4495d0c18d3941d7bec8a793
Rafael Nascimento <rafaeln.dev@gmail.com>
2018-07-15 20:43:25 -0300

    Adicionado arquivos iniciais do projeto

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'Esse é um Recipe test',
+      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'Esse é um Recipe test',
+      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis'
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
